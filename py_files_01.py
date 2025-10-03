81c7a54ab887a4165eaf012c852986da44aeb5d8
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-03 18:42:08 +0200

    txt file write

--- py_files_01.py
+++ py_files_01.py
@@ -1,9 +1,19 @@
 
-first_name = input('Upisite Vase ime: ')
-last_name = input('Upisite Vase prezime: ')
-phone = input('Upisite Vas broj mobitela: ')
-email = input('Upisite Vas email: ')
 
-contact = f'Ime: {first_name};Prezime: {last_name};Phone: {phone};Email: {email}'
+while True:
+    first_name = input('Upisite Vase ime: ')
+    last_name = input('Upisite Vase prezime: ')
+    phone = input('Upisite Vas broj mobitela: ')
+    email = input('Upisite Vas email: ')
 
-print(contact)
+    contact = f'Ime: {first_name};Prezime: {last_name};Phone: {phone};Email: {email}'
+
+    print(contact)
+
+    file_writer = open('contacts.txt', 'w')
+    file_writer.write(contact)
+    file_writer.close()
+
+    next_contact = input('Zelite li unijeti novi kontakt? (Da/Ne): ')
+    if next_contact.lower() != 'da':
+        break
