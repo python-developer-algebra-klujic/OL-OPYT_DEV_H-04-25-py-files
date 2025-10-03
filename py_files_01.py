8fd284c770cb6e8df47c12a3527eebbfffc33802
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-03 18:27:05 +0200

    Update py_files_01.py

---
+++ py_files_01.py
@@ -0,0 +1,9 @@
+
+first_name = input('Upisite Vase ime: ')
+last_name = input('Upisite Vase prezime: ')
+phone = input('Upisite Vas broj mobitela: ')
+email = input('Upisite Vas email: ')
+
+contact = f'Ime: {first_name};Prezime: {last_name};Phone: {phone};Email: {email}'
+
+print(contact)
